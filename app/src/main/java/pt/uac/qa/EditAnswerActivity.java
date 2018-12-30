abdd850ee315a5e2c3ea3b6cb4c7b4ca4c000dd5
Cactusforlife <miguelsardinhapimentel@hotmail.com>
2018-12-30 18:32:42 -0100

    added EditAnswer

--- app/src/main/java/pt/uac/qa/EditAnswerActivity.java
+++ app/src/main/java/pt/uac/qa/EditAnswerActivity.java
@@ -1,4 +1,13 @@
 package pt.uac.qa;
 
-public class EditAnswerActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+    }
 }
